a20c4ea023005db4986db30fe90bb6092c8069d3
ToshaZ <tsterk.hzimmer@gmail.com>
2021-02-21 21:24:15 -0600

    updated ERD files

--- EmployeeSQL/QuickDBD_SQL-export.sql
+++ EmployeeSQL/QuickDBD_SQL-export.sql
@@ -2,65 +2,65 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
+CREATE TABLE "departments" (
     "dept_no" VARCHAR(30)   NOT NULL,
     "dept_name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "emp_title_id" VARCHAR(30)   NOT NULL,
     "title" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
+    CONSTRAINT "pk_titles" PRIMARY KEY (
         "emp_title_id"
      )
 );
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(30)   NOT NULL,
     "birth_date" VARCHAR(30)   NOT NULL,
     "first_name" VARCHAR(50)   NOT NULL,
     "last_name" VARCHAR(50)   NOT NULL,
     "sex" VARCHAR(30)   NOT NULL,
-    "hire_date" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL
 );
 
-CREATE TABLE "Dept_manager" (
+CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(30)   NOT NULL,
     "emp_no" INT   NOT NULL
 );
 
-CREATE TABLE "Dept_emp" (
+CREATE TABLE "dept_emp" (
     "dept_no" VARCHAR(30)   NOT NULL,
     "emp_no" INT   NOT NULL
 );
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("emp_title_id");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("emp_title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
